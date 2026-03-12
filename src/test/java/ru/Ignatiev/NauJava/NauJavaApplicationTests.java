808bc38fc1b63079b39b2fa2aad388100de88e0e
Mysashi <plantariy@gmail.com>
2026-03-12 23:29:19 +0500

    Initial Commit

---
+++ src/test/java/ru/Ignatiev/NauJava/NauJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Ignatiev.NauJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
